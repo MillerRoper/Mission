dfc426b9272d2f32d28fb8060c7825485067c188
RoperMiller <mill22008@byui.edu>
2024-10-07 09:29:29 -0600

    fix git

---
+++ coolpics/debugging/index.js
@@ -0,0 +1,6 @@
+const helloElement = document.getElementById('hello')
+
+helloElement.innerText = 'Hello World!'
+
+const hello2element = document.getElementById('hello2')
+hello2element.innerText = 'Hello world!'
